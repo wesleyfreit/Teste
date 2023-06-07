ac16fc25da0400792325109bb6c62761f239b9b4
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-06-07 20:08:19 -0300

    building

---
+++ database/database.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+class MongoDB {
+    constructor() {
+        this.mongodbURI = process.env.MONGODB_URI;
+    }
+    async connect() {
+        try {
+            await mongoose_1.default.connect(this.mongodbURI);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}
+new MongoDB().connect();
+exports.default = mongoose_1.default;
