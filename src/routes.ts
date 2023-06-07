76766e92a47e671bef92096dc2d1c9f88c3ca79b
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-06-07 19:42:10 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -6,9 +6,13 @@ const router = Router();
 const eventController = new EventController();
 
 router.get("/events", eventController.listEvents);
+
 router.get("/events/:id", eventController.findEvent);
+
 router.post("/events", eventController.saveEvent);
+
 router.put("/events/:id", eventController.updateEvent);
+
 router.delete("/events/:id", eventController.deleteEvent);
 
 export default router;
