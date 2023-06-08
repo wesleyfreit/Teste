8caa3c5b9d9beabeaee2a3fe435e658f35c36ca7
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-06-07 21:28:10 -0300

    a

--- src/models/Event.ts
+++ src/models/Event.ts
@@ -1,25 +1,25 @@
-// import { Schema } from "mongoose";
-// import mongoose from "../../database/database";
+import { Schema } from "mongoose";
+import mongoose from "../../database/database";
 
-// const eventSchema = new Schema(
-//   {
-//     title: String,
-//     startDate: { type: Date, default: Date.now },
-//     endDate: { type: Date, default: Date.now },
-//     description: String,
-//     point: {
-//       type: { type: String },
-//       coordinates: { type: Array },
-//     },
-//   },
-//   { collection: "events" }
-// );
+const eventSchema = new Schema(
+  {
+    title: String,
+    startDate: { type: Date, default: Date.now },
+    endDate: { type: Date, default: Date.now },
+    description: String,
+    point: {
+      type: { type: String },
+      coordinates: { type: Array },
+    },
+  },
+  { collection: "events" }
+);
 
-// eventSchema.index(
-//   { title: "text", description: "text" },
-//   { default_language: "pt", weights: { title: 2, description: 1 } }
-// );
+eventSchema.index(
+  { title: "text", description: "text" },
+  { default_language: "pt", weights: { title: 2, description: 1 } }
+);
 
-// const Event = mongoose.model("Event", eventSchema);
+const Event = mongoose.model("Event", eventSchema);
 
-// export default Event;
+export default Event;
