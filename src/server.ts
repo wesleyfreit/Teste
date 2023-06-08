bc21ab0bd12254cede36723a8c96a10c51bbcd97
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-06-08 13:00:58 -0300

    prepare to deploy

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import router from './routes';
+
+dotenv.config();
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(router);
+
+server.listen({ port: process.env.PORT || 8080 }, () => {
+  console.log('The server is running');
+});
