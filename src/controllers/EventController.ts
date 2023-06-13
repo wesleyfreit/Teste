34b565d8b482a02a702682adb343525b73540645
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-06-13 11:04:23 -0300

    Update EventController.ts

--- src/controllers/EventController.ts
+++ src/controllers/EventController.ts
@@ -20,7 +20,9 @@ export class EventController {
     const body = <iEvent>req.body;
     if (body.title != '' && body.point.coordinates[1] && body.point.coordinates[0]) {
       try {
-        await Event.create(body);
+        const event = await Event.create(body);
+        await redisClient.set(`id-${event.id}`, JSON.stringify(event), { EX: 3600 });
+        await redisClient.del('array-events');
         return res.sendStatus(201);
       } catch (error) {
         return res.sendStatus(400);
@@ -40,9 +42,13 @@ export class EventController {
         if (events.length > 0) return res.json(events);
         else return res.sendStatus(204);
       }
+      const redisEvents = await redisClient.get('array-events');
+      if (redisEvents) return res.json(JSON.parse(redisEvents));
       const events = await Event.find({}, { _id: true, __v: false }).sort({ startDate: -1 });
-      if (events.length > 0) return res.json(events);
-      else return res.sendStatus(204);
+      if (events.length > 0) {
+        await redisClient.set('array-events', JSON.stringify(events), { EX: 3600 });
+        return res.json(events);
+      } else return res.sendStatus(204);
     } catch (error) {
       return res.sendStatus(404);
     }
@@ -51,13 +57,11 @@ export class EventController {
   public async findEvent(req: Request, res: Response) {
     const id = req.params.id;
     try {
-      const redisConsult = await redisClient.get(id);
+      const redisConsult = await redisClient.get(`id-${id}`);
       if (redisConsult) return res.json(JSON.parse(redisConsult));
       const event = await Event.findById(id);
       if (event) {
-        await redisClient.set(id, JSON.stringify(event), {
-          EX: 3600,
-        });
+        await redisClient.set(`id-${id}`, JSON.stringify(event), { EX: 3600 });
         return res.json(event);
       } else return res.sendStatus(204);
     } catch (error) {
@@ -70,14 +74,12 @@ export class EventController {
     const id = req.params.id;
     if (body.title != '' && body.point.coordinates[1] && body.point.coordinates[0]) {
       try {
-        const event = await Event.findById(id);
-        if (event) {
-          const result = await Event.updateOne({ _id: id }, { ...body });
-          if (result) {
-            await redisClient.del(id);
-            return res.sendStatus(200);
-          } else return res.sendStatus(400);
-        } else return res.sendStatus(204);
+        const result = await Event.findOneAndUpdate({ _id: id }, { ...body });
+        if (result) {
+          await redisClient.set(`id-${id}`, JSON.stringify(event), { EX: 3600 });
+          await redisClient.del('array-events');
+          return res.sendStatus(200);
+        } else return res.sendStatus(400);
       } catch (error) {
         return res.sendStatus(404);
       }
@@ -87,14 +89,12 @@ export class EventController {
   public async deleteEvent(req: Request, res: Response) {
     const id = req.params.id;
     try {
-      const event = await Event.findById(id);
-      if (event) {
-        const result = await Event.deleteOne({ _id: id });
-        if (result) {
-          await redisClient.del(id);
-          return res.sendStatus(200);
-        } else return res.sendStatus(400);
-      } else return res.sendStatus(204);
+      const result = await Event.findOneAndRemove({ _id: id });
+      if (result) {
+        await redisClient.del(`id-${id}`);
+        await redisClient.del('array-events');
+        return res.sendStatus(200);
+      } else return res.sendStatus(400);
     } catch (error) {
       return res.sendStatus(404);
     }
